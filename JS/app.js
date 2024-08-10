94ad79cad1ef853fe1cb60edbc5acf1dfde46b81
Jessica Garcia Chaparro <jessicagarciachaparro@gmail.com>
2024-08-10 16:46:07 -0500

    globe's redimention and adjust respect to container

--- JS/app.js
+++ JS/app.js
@@ -1,22 +1,25 @@
-const {json, select, selectAll, geoOrthographic, geoPath, geoGraticule} = d3;
+const { json, select, selectAll, geoOrthographic, geoPath, geoGraticule } = d3;
 const mapSpace = document.getElementById('mapSpace');
 
+// Definir los países de interés
 const countriesOfInterest = [
     "United Kingdom", "Colombia", "Egypt", "Germany", "Austria",
     "United States", "France", "Italy"
 ];
 
+// Cargar los datos GeoJSON completos que permiten el renderizado de un globo terraqueo
 json('../JSON/custom.geo.json').then(data => {
     console.log(data); // Verifica que los datos se carguen correctamente
     init(data);
 });
 
-let geojson, globe, projection, path, graticule, isMouseDown = false, rotation = {x:0, y:0};
+//creacion de variables necesarias para la proyeccion del globo terraqueo
+let geojson, globe, projection, path, graticule, isMouseDown = false, rotation = {x: 0, y: 0};
 
-const globeSize = {
-    w: mapSpace.clientWidth,
-    h: mapSpace.clientHeight
-};
+// Definición de tamaño del SVG (globo) y márgenes
+const svgWidth = 600;  // Ancho deseado para el SVG
+const svgHeight = 400; // Alto deseado para el SVG
+const margin = 50;     // Margen para evitar recortes
 
 const init = data => {
     geojson = data;
@@ -26,16 +29,18 @@ const init = data => {
     createDraggingEvents(); 
 };
 
+//Función que define como se va a crear y proyectar el globo
 const drawGlobe = () => {
     globe = d3.select('#mapSpace')
         .append('svg')
-        .attr('width', globeSize.w)
-        .attr('height', globeSize.h)
+        .attr('width', svgWidth + margin * 2)
+        .attr('height', svgHeight + margin * 2)
         .attr('class', 'world');
 
     projection = geoOrthographic()
-        .fitSize([globeSize.w, globeSize.h], geojson)
-        .translate([globeSize.w / 2, globeSize.h / 2]);
+        .fitSize([svgWidth, svgHeight], geojson)
+        .translate([(svgWidth + margin) / 2, (svgHeight + margin) / 2])
+        .scale(svgWidth / 3);  // Ajusta la escala para reducir el tamaño del globo
 
     path = geoPath().projection(projection);
 
@@ -60,6 +65,7 @@ const drawGraticule = () => {
         .attr('stroke', '#232323');
 };
 
+//comportamientos al hacer hover sobre determinados paises
 const createHoverEffect = () => {
     globe.selectAll('.country')
         .on('mouseover', function(event, d) {
@@ -83,7 +89,7 @@ const createDraggingEvents = () => {
         .on('mouseup', () => isMouseDown = false)
         .on('mousemove', (e) => {
             if (isMouseDown) {
-                const {movementX, movementY} = e;
+                const { movementX, movementY } = e;
                 rotation.x += movementX / 2; 
                 rotation.y += movementY / 2; 
                 projection.rotate([rotation.x, rotation.y]);
@@ -91,4 +97,17 @@ const createDraggingEvents = () => {
                 selectAll('.graticule').attr('d', path(graticule()));
             }
         });
+};
+
+
+//Información a mostrar cada vez que se hace hover sobre el pais de alguna cientifica
+const countryInfo = {
+    "United Kingdom": "Ada Lovelace nació en Londres, Inglaterra en 1815",
+    "Colombia": "Alexandra Olaya Castro nació en Bogotá, Colombia en 1976",
+    "Egypt": "Hipatia de Alejandria nació en Alejandria, Egipto entre el 355 y 370 d.C. y Dorothy Crowfoot Nació en 1910 en El Cairo, Egipto, hija de un arqueólogo británico.",
+    "Germany": "Emmy Noether nació en 1882 en Erlangen, Alemania,",
+    "Austria": "Hedy Lamarr nació en Viena, Austria en 1914 y Lise Meitner nació en la misma ciudad en 1878",
+    "United States": "Katherine Johnson nació en 1918 en White Sulphur Springs, Virginia Occidental y Rachel Carson Carson en 1907 en Springdale, Pensilvania (Estados Unidos) ",
+    "France": "Marie Lavoisier nació en París, Francia en 1758",
+    "Italy": "Rita Levi nació en Turin, Italia en 1909"
 };
